8113b433afbcb81e85983189aa0fcd4f6a45dd47
Kevin Montero <kevinwilding1997@gmail.com>
2019-11-25 22:08:09 -0700

    Assignment done

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-const express 
+const express = require("express");
+const mc = require("./controllers/messages_controller");
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.use(express.static(__dirname + '/../public/build'));
+
+
+const messagesBaseUrl = "/api/messages";
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}/:id`, mc.update);
+app.delete(`${messagesBaseUrl}/:id`, mc.delete);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
